5f72464e29559ad2d683bcc0bb398a1a7a959243
lookaiz <mmonchy@palo-it.com>
2021-06-14 22:25:28 +0200

    Strip comments

--- src/test/java/com/lookaiz/kyu4/StripCommentsTest.java
+++ src/test/java/com/lookaiz/kyu4/StripCommentsTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StripCommentsTest {
 
     @Test
-    public void stripComments() throws Exception {
+    public void stripComments() {
         assertEquals(
                 "apples, pears\ngrapes\nbananas",
                 StripComments.stripComments( "apples, pears # and bananas\ngrapes\nbananas !apples", new String[] { "#", "!" } )
