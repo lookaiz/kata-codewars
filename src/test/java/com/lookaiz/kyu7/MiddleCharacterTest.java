327f52b62b10122ca92af193b921e2e23344b4d2
lookaiz <mmonchy@palo-it.com>
2021-06-14 09:09:52 +0200

    Middle character

---
+++ src/test/java/com/lookaiz/kyu7/MiddleCharacterTest.java
@@ -0,0 +1,20 @@
+package com.lookaiz.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MiddleCharacterTest {
+
+    @Test
+    public void evenTests() {
+        assertEquals("es", MiddleCharacter.getMiddle("test"));
+        assertEquals("dd", MiddleCharacter.getMiddle("middle"));
+    }
+
+    @Test
+    public void oddTests() {
+        assertEquals("t", MiddleCharacter.getMiddle("testing"));
+        assertEquals("A", MiddleCharacter.getMiddle("A"));
+    }
+}
